cd475b1e73e08091d5d6bced0a8772bf2027cb64
carmen-rr <=>
2022-10-27 14:32:12 -0300

    prueba

---
+++ src/screens/Comments/Comments.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import  {View, Text} from 'react-native'; 
+
+class Comments extends Component {
+    render () {
+    return (
+        <View>
+            <Text>Estructura basica de: Comments</Text>
+        </View>
+    )
+    }
+}
+
+export default Comments; 
