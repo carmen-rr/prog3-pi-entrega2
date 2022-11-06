c83097544f0344d928e4ebce71954dd23a38bc6a
carmen-rr <crodriguezreynoso@udesa.edu.ar>
2022-11-05 21:39:00 -0300

    empece a armar posts + detalles de profile

--- src/screens/Post/Post.js
+++ src/screens/Post/Post.js
@@ -1,14 +1,74 @@
 import React, {Component} from "react";
-import  {View, Text} from 'react-native'; 
+import {db, auth} from '../../firebase/config'
+import  {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native'; 
+
 
 class Post extends Component {
+    constructor(){
+        super()
+        this.state={
+            comment:''
+        }
+    }
+
+    //creando posteos en una coleccion de firebase
+    sendComment(comment){
+        db.collection('posts').add({
+            owner: auth.currentUser.email,
+            date: Date.now,
+            comment: this.state.comment,
+        })
+        .then(()=> (this.setState({comment: ''}))) //que el comentario vuelva a ser vacio una vez que se envia correctamente
+        .catch(err => console.log(err))
+    }
+
     render () {
     return (
         <View>
-            <Text>Estructura basica de: Post</Text>
+            <Text>¡Crea tu posteo!</Text>
+
+            <TextInput 
+                keyboardType='default'
+                placeholder='Escribi tu comentario...'
+                onChangeText={text => this.setState({comment: text})} //cambia el estado del comentario
+                style={styles.input}
+                value={this.state.comment}
+            /> 
+
+                <TouchableOpacity onPress={()=> this.sendComment(this.state.comment) }  style={styles.button}>
+                    <Text>Enviar mi comentario</Text>
+                </TouchableOpacity>
+
+                
         </View>
     )
     }
 }
 
+
+const styles =StyleSheet.create ({
+    input: {
+        height: 60, 
+        borderWidth: 1, 
+        margin: 5,
+        borderRadius: 10,
+        textAlign:'center',
+
+
+
+
+    },
+    button: {
+        padding:10,
+        borderColor:`#5f9ea0`,
+        borderWidth: 2,
+        borderRadius:20,
+        backgroundColor:`#5f9ea0`,
+        textAlign:'center',
+        
+        
+    }
+})
+
 export default Post; 
+
--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,15 +1,37 @@
 import React, {Component} from "react";
 import  {View, Text, TouchableOpacity, StyleSheet} from 'react-native'; 
-import {auth} from '../../firebase/config'
+import {auth, db} from '../../firebase/config'
 
 class Profile extends Component {
     
     constructor(props){
         super(props)
+        this.state = {
+            //allComments = []
+        }
     }
     signOut(){
         auth.signOut()
         this.props.navigation.navigate('Login')
+    }
+
+
+    //obteniendo los comentarios 
+
+    
+    componentDidMount(){
+        db.collection ('posts').onSnapshot(docs => {console.log(docs)
+        /*
+        let comments = []
+        docs.forEach (doc => {
+            comments.push({
+                 id: doc.id, 
+                data: doc.data(), 
+            })
+         })
+         this.setState({allComments : comments}, () => console.log(this.state.allComments))
+         */
+        })
     }
     
     render () {
@@ -27,9 +49,16 @@ class Profile extends Component {
 const styles = StyleSheet.create({
     button:{
         padding:10,
-        borderColor:'red',
-        borderWidth: 2
+        borderColor:'black',
+        borderWidth: 2,
+        borderRadius:20,
+        backgroundColor:'#dc143c',
+        textAlign:'center',
+
+
+
     }
 })
 
 export default Profile; 
+
